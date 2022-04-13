ec9098bfe46123c40912dd8105f43765de1b272d
YaroslavAndreev <94409822+YaroslavAndreev@users.noreply.github.com>
2022-04-13 21:05:51 +0300

    Some bugs were fixed. 29 tests have passed.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,15 +6,16 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated and request.user.role == User.ADMIN:
             return True
-
         elif request.method in permissions.SAFE_METHODS:
             return True
 
 
 class IsUserOrAdminOrModerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+        return (
+            request.user.is_authenticated
+            or request.method in permissions.SAFE_METHODS
+        )
 
     def has_object_permission(self, request, view, obj):
         if request.user.is_authenticated:
