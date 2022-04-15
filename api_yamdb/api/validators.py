6678b6fd7efb5b94ac91329012c15dc6f028c7f7
Евгений Быков <bykovevg@yandex.ru>
2022-04-15 21:02:33 +0300

    third pull

--- api_yamdb/api/validators.py
+++ api_yamdb/api/validators.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from reviews.models import User
 
+
 def username_exists(username):
     if not User.objects.filter(username=username).exists():
         raise serializers.ValidationError(
