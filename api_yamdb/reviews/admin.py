5d473293df129f2c16006f44fd948f850243cba9
Евгений Быков <bykovevg@yandex.ru>
2022-04-21 14:20:20 +0300

    before second review

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,11 +1,82 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from .models import Category, Comment, Genre, Review, Title, User
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Title)
-admin.site.register(Genre)
-admin.site.register(Category)
-admin.site.register(Review)
-admin.site.register(Comment)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+        'role'
+    )
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name'
+    )
+    list_editable = ('role',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'year',
+        'category'
+    )
+    list_filter = ('name', 'year')
+    search_fields = ('name', 'year',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_filter = ('name', )
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'text',
+        'author',
+        'score',
+        'pub_date'
+    )
+    list_filter = ('author', 'score',)
+    search_fields = ('title', 'text', 'author', 'score',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'text',
+        'author',
+        'pub_date',
+        'review'
+    )
+    list_filter = ('author', 'pub_date',)
+    search_fields = (
+        'text',
+        'author',
+        'pub_date'
+    )
+    empty_value_display = '-пусто-'
